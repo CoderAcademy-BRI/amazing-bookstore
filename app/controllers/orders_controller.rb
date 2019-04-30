80670277dc11c142c44062810edbe8f3a0ddb28b
Matt Stubbs <matt@yellowrobot.com.au>
2019-04-30 13:00:00 +1000

    Add Stripe for payments

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,25 @@ class OrdersController < ApplicationController
   def new
     the_book_id = params[:book_id]
     @book = Book.find(the_book_id)
+
+    Stripe.api_key = 'sk_test_pxd9AQJuQrOgGS3SLQj9ZxaD00oSsPY4rj'
+
+    
+
+    @stripe_checkout_session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: @book.title,
+        description: "A great read by #{@book.author.name}",
+        amount: (@book.price * 100).to_i,
+        currency: 'aud',
+        quantity: 1,
+      }],
+      success_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/cancel',
+    )
+  end
+
+  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'orders/new'
+
+  get 'success', to: "orders#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :authors do
     resources :books
